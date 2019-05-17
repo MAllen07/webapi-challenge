faa269e7e75458d642738a1024b2d1859fc9f0f1
MAllen07 <mrsmallen07@gmail.com>
2019-05-17 12:56:21 -0400

    added additional dependencies needed, adjusted index file to bring in server, dynamic port and listening port

--- index.js
+++ index.js
@@ -12,3 +12,16 @@ I need this code, just don't know where, perhaps should make some middleware, do
 
 Go code!
 */
+
+
+const envReader = require('dotenv').config();
+// Bringing in server.
+const server = require('./server.js');
+
+// Dynamic port.
+const port = process.env.PORT || 5000
+
+// server listening on dynamic port or LH:5000
+server.listen(port, () => {
+    console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
+});
---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const server = express();
+
+const actionsRouter = require("./actions/actions-router.js");
+const projectsRouter = require("./projects/projects-router.js");
+
+server.use(express.json());
+server.use("/api/actions", actionsRouter);
+server.use("/api/projects", projectsRouter);
+
+server.get("/", (req, res) => {
+  res.send({
+    welcome: process.env.HELLO
+  });
+});
+
+module.exports = server;
